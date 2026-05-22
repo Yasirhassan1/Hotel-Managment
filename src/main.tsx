67d74eb76fe4dfca35ad1251c35201d7ab6d0ca7
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-05-22 23:16:14 +0500

    chore: update packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react';
-import App from './App.tsx';
-import './index.css';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-	throw new Error('Root not found');
+	throw new Error("Root not found");
 }
 
 const root = createRoot(rootElement);
